7de08545e148bf4b242c6d35435a96b7d01a40ed
Thada Wangthammang <mildronize@gmail.com>
2020-11-27 23:02:57 +0700

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import TestComponent from "./TestComponent";
+import {editor} from 'monaco-editor';
+import {activateFormatting} from "./formatting";
+import {setWordDefinitionFor, TextEditor} from "./vscode-monaco";
+import {activateListEditing} from "./listEditing";
+import {activateCompletion} from "./completion";
+import {activateTableFormatter} from "./tableFormatter";
 
-export { TestComponent };
+import {activateMarkdownMath} from "./markdown.contribution";
+
+export class MonacoMarkdownExtension {
+    activate(editor: editor.IStandaloneCodeEditor) {
+        let textEditor = new TextEditor(editor)
+
+        activateFormatting(textEditor)
+        activateListEditing(textEditor)
+        activateCompletion(textEditor)
+        activateTableFormatter(textEditor)
+
+        // Allow `*` in word pattern for quick styling
+        setWordDefinitionFor(textEditor.languageId,
+            /(-?\d*\.\d\w*)|([^\!\@\#\%\^\&\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s\，\。\《\》\？\；\：\‘\“\’\”\（\）\【\】\、]+)/g
+        );
+    }
+}
+
+activateMarkdownMath()
